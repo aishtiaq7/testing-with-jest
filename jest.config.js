1812aa9bfbd4ec8f04cd9f4a69fe8020cca11b57
aishtiaq <awshaf_ishtiaque@sfu.ca>
2024-09-15 15:17:23 -0700

    added more test cases jest.config file

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  testEnvironment: "node", // Specifies the testing environment, e.g., Node.js or jsdom for browser-like environments.
+  testMatch: ["**/*.test.js"], // Matches test files with '.test.js' suffix.
+  verbose: true, // Provides detailed test results in the output.
+  collectCoverage: true, // Enables code coverage collection to track how much of your code is tested.
+};
--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,22 @@
 const sum = require('./sum');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
+describe("Grouped tests", ()=>{
+  test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+  
+  test(' -7 + -10 to equal -17', () => {
+    expect(sum(-7, -10)).toBe(-17);
+  });
+})
+
+
+describe("Another Group", ()=>{
+  it('adds 2+2 should be <5', () => {
+    expect(sum(2, 2)).toBeLessThan(5);
+  });
+  
+  test(' 10+10 should be more than 9', () => {
+    expect(sum(10,10)).toBeGreaterThan(9);
+  });
+})
